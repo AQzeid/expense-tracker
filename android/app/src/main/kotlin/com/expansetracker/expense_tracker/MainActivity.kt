2a8fd24205135b0293cf284d4b81bf1e2fe5eb18
AQzeid <ashiquesaeed@gmail.com>
2024-04-22 10:54:09 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/expansetracker/expense_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.expansetracker.expense_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/home_page/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Home Page'),
+            // Total expense details widget
+            //
+          ],
+        ),
+      ),
+    );
+  }
+}
